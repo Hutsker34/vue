149e5db1d56891db659c18a362f2e813ea2c5f14
viacheslav34 <77201329+viacheslav34@users.noreply.github.com>
2022-02-19 14:56:30 +0300

    ю

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/лыжник.png">
+  <HelloWorld msg="Welcome to Vue.js"/>
+  <Todo msg ="Welcom to Todo" img = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Todo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1 class ='h1'>{{input}}</h1>
+        <input v-model = 'input'>
+        <img :src = "img" width = '200' @click = "sayHellow" >
+    </div>
+</template>
+
+
+
+
+<script>
+export default {
+    name: 'Todo',
+    props: {
+        msg: String,
+        img: String
+    },
+    data(){
+        return {input: ''}
+    },
+    methods: {
+        sayHellow(){
+            alert('Hellow')
+        },
+        
+    },
+}
+</script>
+
+
+
+
+<style>
+    .h1{
+        color: #40c8b0
+    }
+</style>
